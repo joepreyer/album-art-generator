4b1353243906811fa8e79453c6c8a7069e0ae59c
Joe Preyer <joepreyer@gmail.com>
2023-04-28 19:41:06 +0100

    Fix type errors

--- src/hooks/use-video-generator.ts
+++ src/hooks/use-video-generator.ts
@@ -1,69 +1,66 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export default function useVideoGenerator() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [video, setVideo] = useState(null);
-    const [isStarting, setIsStarting] = useState(false);
+    const [isLoading, setIsLoading] = useState(false)
+    const [video, setVideo] = useState(null)
+    const [isStarting, setIsStarting] = useState(false)
 
-    const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+    const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
-    const summariseLyrics = async (lyrics) => {
-        const response = await fetch("/api/openapi", {
-            method: "POST",
+    const summariseLyrics = async (lyrics: string) => {
+        const response = await fetch('/api/openapi', {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 text: lyrics,
             }),
-        });
+        })
         if (response.status !== 200) {
-            console.error("API Error: ", response.status);
-            throw new Error("API Error: " + response.status);
+            console.error('API Error: ', response.status)
+            throw new Error('API Error: ' + response.status)
         }
-        let data = await response.json();
+        let data = await response.json()
         return data.result.choices[0].text
     }
 
-    const generateVideo = async (lyrics, stylePrompt) => {
+    const generateVideo = async (lyrics: string, stylePrompt?: string) => {
         setIsLoading(true)
         try {
-            const summarisedLyrics = await summariseLyrics(lyrics);
-            const response = await fetch("/api/predictions", {
-                method: "POST",
+            const summarisedLyrics = await summariseLyrics(lyrics)
+            const response = await fetch('/api/predictions', {
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     prompt: summarisedLyrics,
                     style: stylePrompt,
                 }),
-            });
-            let prediction = await response.json();
+            })
+            let prediction = await response.json()
             if (response.status !== 201) {
-                console.error("API Error: ", prediction.detail);
-                alert("API Error: " + prediction.detail) //TODO replace with toast using mui
-                return;
+                console.error('API Error: ', prediction.detail)
+                alert('API Error: ' + prediction.detail) //TODO replace with toast using mui
+                return
             }
 
-            while (
-                prediction.status !== "succeeded" &&
-                prediction.status !== "failed"
-            ) {
-                if (prediction.status === "starting") setIsStarting(true);
-                await sleep(1000);
-                const response = await fetch("/api/predictions/" + prediction.id);
-                prediction = await response.json();
+            while (prediction.status !== 'succeeded' && prediction.status !== 'failed') {
+                if (prediction.status === 'starting') setIsStarting(true)
+                await sleep(1000)
+                const response = await fetch('/api/predictions/' + prediction.id)
+                prediction = await response.json()
                 if (response.status !== 200) {
-                    console.error("API Error: ", prediction.detail);
-                    alert("API Error: " + prediction.detail) //TODO replace with toast using mui
-                    return;
+                    console.error('API Error: ', prediction.detail)
+                    alert('API Error: ' + prediction.detail) //TODO replace with toast using mui
+                    return
                 }
             }
-            setVideo(prediction);
+            setVideo(prediction)
             setIsLoading(false)
         } catch (error) {
-            console.error("API error: ", error)
+            console.error('API error: ', error)
         }
     }
 
