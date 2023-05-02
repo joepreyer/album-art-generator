c414d55e0b86f82da7dc44690b9b10dd36e812a9
Joe Preyer <joepreyer@gmail.com>
2023-05-02 14:35:49 +0100

    Update error handling for generate image

--- src/hooks/use-image-generator.ts
+++ src/hooks/use-image-generator.ts
@@ -6,32 +6,39 @@ export default function useVideoGenerator() {
     const [image, setImage] = useState(null)
 
     const generateImage = async (lyrics: string, stylePrompt?: string) => {
-        if (!lyrics) {
-            console.error('Lyrics must be provided')
-            throw new Error('Lyrics must be provided')
-        }
-        setIsLoading(true)
-        const response = await fetch('/api/openapi/generate-image', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                lyrics: lyrics,
-                stylePrompt: stylePrompt,
-            }),
-        })
-        if (!response.ok) {
-            const errorResponse = await response.json()
-            const errorMessage =
-                errorResponse.error.message || 'Unknown error occurred while generating image - please try again.'
-            console.error('Error generating image', errorMessage)
+        try {
+            if (!lyrics) {
+                console.error('Lyrics must be provided')
+                throw new Error('Lyrics must be provided')
+            }
+            setIsLoading(true)
+            const response = await fetch('/api/openapi/generate-image', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    lyrics: lyrics,
+                    stylePrompt: stylePrompt,
+                }),
+            })
+            if (!response.ok) {
+                const errorResponse = await response.json()
+                const errorMessage =
+                    errorResponse.error.message ||
+                    'Unknown error occurred while generating image - please try again.'
+                console.error('Error generating image', errorMessage)
+                setIsLoading(false)
+                throw new Error(errorMessage)
+            }
+            let data = await response.json()
+            setImage(data.imageURL)
             setIsLoading(false)
-            throw new Error(errorMessage)
+        } catch (error) {
+            console.error('There was an error when generating the image')
+            console.error(error)
+            setIsLoading(false)
         }
-        let data = await response.json()
-        setImage(data.imageURL)
-        setIsLoading(false)
     }
 
     return { isLoading, image, generateImage }
