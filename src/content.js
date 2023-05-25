8c2f3dc228fb372a47d4620715be535a5e3a3440
Joseph Preyer <joepreyer@gmail.com>
2023-05-25 09:02:38 +0200

    Update content.js

--- src/content.js
+++ src/content.js
@@ -10,13 +10,14 @@ export const styleOptions = [
 ]
 
 export const pages = [
-    { label: 'Album Art Generator', link: '/' },
+    { label: 'Canvas Generator', link: '/' },
     { label: 'About', link: '/about' },
+    { label: 'Contact', link: `mailto:${process.env.NEXT_PUBLIC_EMAIL_ADDRESS}?subject=Hey My Spotify Canvas!` },
 ]
 
 export const aboutParagraphs = [
-    'Artify is a tool to for musicians to generate album artwork from their song lyrics using AI, in a cost-effective manner.',
+    'My Spotify Canvas is a tool to for musicians to generate album artwork from their song lyrics using AI, in a cost-effective manner.',
     'AI is empowering artists to express their creativity in new, exciting ways. Our mission is to provide accessible, cutting-edge technology that enables musicians and other creatives to reach their full potential.',
     'Thank you for using our tool!',
-    "If you have any questions or feedback, please don't hesitate to reach out to us. You can unlock the email address below for $0.1 using Mash.",
+    "If you have any questions or feedback, please don't hesitate to reach out to us at:",
 ]
